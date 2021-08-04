6530496e96654413c5f1c55aa73679ba5df203bc
Rithik Bhandari <rithikbhandari895@gmail.com>
2021-08-04 19:59:57 +0530

    🌐 added progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,20 @@ import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import Layout from "../components/layout/layout";
 import Head from "next/head";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import Router from "next/router";
+
+NProgress.configure({
+    minimum: 0.35,
+    easing: 'ease',
+    speed: 800,
+    showSpinner: false,
+});
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const MyApp = ({Component, pageProps}: AppProps) => {
     return (
